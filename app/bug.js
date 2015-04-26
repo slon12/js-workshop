59d675c3840edc316808f0082a95170c91d683c3
ania <ania_w@outlook.com>
2015-04-26 14:45:36 +0200

    ch-2-answer

--- app/bug.js
+++ app/bug.js
@@ -1,31 +1,26 @@
+var joke = 'JavaScript walked into a bar....';
+var toldJoke = false;
+var $punchline = 'Better watch out for those semi-colons.';
+//var result;
 
-    var joke = "JavaScript walked into a bar....';
-    var toldJoke = "false";
-    var $punchline = 'Better watch out for those semi-colons.'
-    var result
+if (toldJoke === true) {
+    console.log($punchline);
+} else {
+    console.log(joke);
+}
 
-    if (toldJoke === true) {
-        Console.log($punchline);
-    } else {
-        console.log(joke);
-    }
+//var zipCode = 98104;
+var joesFavoriteMovie = 'Forbidden Planet';
+var movieTicket$ = 9;
 
-    var zip_code = 98104;
-    var joesFavorite_Movie = Forbidden Planet;
-    var movieTicket$ = 9;
+if (movieTicket$ >= 9) {
+    console.log('Too much!');
+} else {
+    console.log('We are going to see ' + joesFavoriteMovie);
+}
 
-    if (movieTicket$ >= 9) {
-        console.log("Too much!");
-    } else {
-        console.log("We're going to see " + joesFavoriteMovie);
-    }
+var a = 5;
+var b = '5';
+var equal;
 
-    var a = 5;
-    var b = "5";
-    var equal;
-
-    if (a == b) {
-        equal = true;
-    } else {
-        equal = false;
-    }
+equal = a === b;
